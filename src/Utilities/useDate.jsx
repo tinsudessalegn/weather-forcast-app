63743d13053324dc034ff033316cef0eb3f40917
tinsudessalegn <tinsae.yirgu@ethernet.edu.et>
2024-01-23 23:42:47 +0300

    providing hooks for time and date

---
+++ src/Utilities/useDate.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react"
+
+
+export const useDate = ()=> {
+    const locale = 'en'
+    const [today, setTodayDate] = useState(new Date())
+
+    useEffect(()=>{
+        const timer = setInterval(()=> {
+            setTodayDate(new Date())
+        }, 60*1000)
+
+        return ()=> {
+            clearInterval(timer)
+        }
+    }, [])
+
+    const day = today.toLocaleDateString(locale, {weekday: 'long'})
+
+    const date = `${day}, ${today.getDate()}, ${today.toLocaleDateString(locale, {month: 'long'})}\n\n`
+
+    const time = today.toLocaleDateString( locale, {hour: 'numeric', hour12: true, minute: 'numeric' })
+
+    return { date, time}
+}
