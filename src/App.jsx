d3c8d5fb7ec203259f3a44a9b003237eef8f65ca
tinsudessalegn <tinsae.yirgu@ethernet.edu.et>
2024-01-24 00:00:44 +0300

    defining nav items: search and nav links

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-
+import { useState } from "react"
+import search from './assets/icons/search.svg'
 
 function App() {
-
+  const [city, setCity] = useState('')
+  const submitCity = ()=> {
+    setCity('')
+  }
   return (
     <>
-      Weather App
+      <div className='w-full h-screen text-white px-8'>
+        <nav className='w-full p-3 flex justify-between items-center'>
+          <h1 className='font-bold tracking-wide text-3xl'>Weather App</h1>
+          <h3 className='font-bold tracking-wide text-xl'> Favorite Cities</h3>
+
+          <div className='bg-white w-[15rem] overflow-hidden shadow-2xl rounded flex items-center p-2 gap-2'>
+          <img src={search} alt="search" className='w-[1.5rem] h-[1.5rem]' />
+          <input  onKeyUp={(e)=> {
+                    if(e.key === 'Enter'){
+                      submitCity()
+                    }
+                  }}    
+                  placeholder='Search city' className='focus:outline-none w-full text-[#212121] text-lg' value={city}
+                  onChange={e => setCity(e.target.value)}/>
+          </div>
+        </nav>
+      </div>
     </>
   )
 }
